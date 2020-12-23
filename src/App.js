1493bc9eb2aa6628645812150deda25f46d160b9
kenglxn <ken@glxn.net>
2020-12-23 17:43:14 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
-import logo from './logo.svg';
-import { Button } from 'antd';
-import './App.css';
+import logo from "./logo.svg";
+import { Button } from "antd";
+import "./App.css";
+import { useHistory, useLocation } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
+  let history = useHistory();
+  let location = useLocation();
+
+  function go(to) {
+    history.push(to);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button type="primary" shape="circle">42</Button>
       </header>
-
-      asd
+      <Button
+        type={location.pathname === "/" ? "primary" : "ghost"}
+        shape="round"
+        onClick={() => go("/")}
+      >
+        Home
+      </Button>
+      <Button
+        type={location.pathname === "/about" ? "primary" : "ghost"}
+        shape="round"
+        onClick={() => go("/about")}
+      >
+        About
+      </Button>
+      <Button
+        type={location.pathname === "/dash" ? "primary" : "ghost"}
+        shape="round"
+        onClick={() => go("/dash")}
+      >
+        Dashboard
+      </Button>
+      <Switch>
+        <Route exact path="/">
+          <h2>Home</h2>
+        </Route>
+        <Route path="/about">
+          <h2>About</h2>
+        </Route>
+        <Route path="/dash">
+          <h2>Dashboard</h2>
+        </Route>
+      </Switch>
     </div>
   );
 }
