037eaec9aaecbe7fae169a5f1d3371b7cfc320fe
kenglxn <ken@glxn.net>
2021-01-02 19:48:44 +0100

    fix homepage

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+# inspired by https://gist.github.com/nobuoka/d0f088df57d50e4cda1a
+
+npm run build
+git add -f ./build
+$treeObjId = git write-tree --prefix=build
+git reset -- ./build
+$commitId = git commit-tree -p gh-pages -m "new deploy" $treeObjId
+git update-ref refs/heads/gh-pages $commitId
+git push origin gh-pages
