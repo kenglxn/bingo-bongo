0fb81bfeb81659855acc3857e249dc500604d730
kenglxn <ken@glxn.net>
2021-01-03 16:39:44 +0100

    fix layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import { Link, useHistory } from "react-router-dom";
-import { Button, Card, Tooltip, List } from "antd";
+import { Divider, Button, Row, Col, Tooltip, List } from "antd";
 import { useStorage } from "context/Storage";
 import { FileAddOutlined, CheckCircleOutlined } from "@ant-design/icons";
 
@@ -8,39 +8,44 @@ function Home() {
   const { state: games } = useStorage();
 
   return (
-    <Card>
-      <Tooltip title="opprett nytt spill">
-        <Button
-          data-testid="opprett nytt spill"
-          onClick={() => history.push("/game")}
-          type="primary"
-          icon={<FileAddOutlined />}
-        >
-          Lag spill
-        </Button>
-      </Tooltip>
+    <>
+      <Divider>Bingo</Divider>
+      <Row justify="center" gutter={[10, 10]}>
+        <Col flex="0 1 80vw">
+          <Tooltip title="opprett nytt spill">
+            <Button
+              data-testid="opprett nytt spill"
+              onClick={() => history.push("/game")}
+              type="primary"
+              icon={<FileAddOutlined />}
+            >
+              Lag spill
+            </Button>
+          </Tooltip>
 
-      <List
-        locale={{ emptyText: <></> }}
-        itemLayout="horizontal"
-        dataSource={games}
-        pagination={{
-          pageSize: 10,
-          hideOnSinglePage: true,
-        }}
-        renderItem={(game) => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={<CheckCircleOutlined />}
-              title={<Link to={`/game/${game.id}`}>{game.name}</Link>}
-              description={`${game.name}: ${new Date(
-                game.created_at
-              ).toLocaleString()}`}
-            />
-          </List.Item>
-        )}
-      />
-    </Card>
+          <List
+            locale={{ emptyText: <></> }}
+            itemLayout="horizontal"
+            dataSource={games}
+            pagination={{
+              pageSize: 10,
+              hideOnSinglePage: true,
+            }}
+            renderItem={(game) => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<CheckCircleOutlined />}
+                  title={<Link to={`/game/${game.id}`}>{game.name}</Link>}
+                  description={`${game.name}: ${new Date(
+                    game.created_at
+                  ).toLocaleString()}`}
+                />
+              </List.Item>
+            )}
+          />
+        </Col>
+      </Row>
+    </>
   );
 }
 
